319020b08ebcf71b55b5648e759c130467ff242a
Vinícius Bonatti Benner <vinicius101313@gmail.com>
2022-09-08 08:36:58 -0300

    hehe

--- script.js
+++ script.js
@@ -1,9 +1,79 @@
-import * as Tone from 'tone'
 
-function teste(){
-    const synth = new Tone.Synth().toDestination();
 
-    //play a middle 'C' for the duration of an 8th note
-    synth.triggerAttackRelease("C4", "8n");
+function C() {
+    const synth = new Tone.Synth().toMaster()
+    synth.triggerAttackRelease('C4', '8n')
 }
-teste()
+function C() {
+    const synth = new Tone.Synth().toMaster()
+    synth.triggerAttackRelease('C4', '8n')
+}
+function C() {
+    const synth = new Tone.Synth().toMaster()
+    synth.triggerAttackRelease('C4', '8n')
+}
+function C() {
+    const synth = new Tone.Synth().toMaster()
+    synth.triggerAttackRelease('C4', '8n')
+}
+function C() {
+    const synth = new Tone.Synth().toMaster()
+    synth.triggerAttackRelease('C4', '8n')
+}
+function C() {
+    const synth = new Tone.Synth().toMaster()
+    synth.triggerAttackRelease('C4', '8n')
+}
+function C() {
+    const synth = new Tone.Synth().toMaster()
+    synth.triggerAttackRelease('C4', '8n')
+}
+function C() {
+    const synth = new Tone.Synth().toMaster()
+    synth.triggerAttackRelease('C4', '8n')
+}
+function C() {
+    const synth = new Tone.Synth().toMaster()
+    synth.triggerAttackRelease('C4', '8n')
+}
+function C() {
+    const synth = new Tone.Synth().toMaster()
+    synth.triggerAttackRelease('C4', '8n')
+}
+function C() {
+    const synth = new Tone.Synth().toMaster()
+    synth.triggerAttackRelease('C4', '8n')
+}
+function C() {
+    const synth = new Tone.Synth().toMaster()
+    synth.triggerAttackRelease('C4', '8n')
+}
+function C() {
+    const synth = new Tone.Synth().toMaster()
+    synth.triggerAttackRelease('C4', '8n')
+}
+function C() {
+    const synth = new Tone.Synth().toMaster()
+    synth.triggerAttackRelease('C4', '8n')
+}
+function C() {
+    const synth = new Tone.Synth().toMaster()
+    synth.triggerAttackRelease('C4', '8n')
+}
+function C() {
+    const synth = new Tone.Synth().toMaster()
+    synth.triggerAttackRelease('C4', '8n')
+}
+function C() {
+    const synth = new Tone.Synth().toMaster()
+    synth.triggerAttackRelease('C4', '8n')
+}
+function C() {
+    const synth = new Tone.Synth().toMaster()
+    synth.triggerAttackRelease('C4', '8n')
+}
+function C() {
+    const synth = new Tone.Synth().toMaster()
+    synth.triggerAttackRelease('C4', '8n')
+}
+
