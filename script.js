0111974afa0de46bd03bd8a16c6e1c517ac54f31
Vinícius Bonatti Benner <vinicius101313@gmail.com>
2022-09-08 08:45:46 -0300

    HEHEHEEHEH

--- script.js
+++ script.js
@@ -4,76 +4,56 @@ function C() {
     const synth = new Tone.Synth().toMaster()
     synth.triggerAttackRelease('C4', '8n')
 }
-function C() {
+function D() {
     const synth = new Tone.Synth().toMaster()
-    synth.triggerAttackRelease('C4', '8n')
+    synth.triggerAttackRelease('D4', '8n')
 }
-function C() {
+function E() {
     const synth = new Tone.Synth().toMaster()
-    synth.triggerAttackRelease('C4', '8n')
+    synth.triggerAttackRelease('E4', '8n')
 }
-function C() {
+function F() {
     const synth = new Tone.Synth().toMaster()
-    synth.triggerAttackRelease('C4', '8n')
+    synth.triggerAttackRelease('F4', '8n')
 }
-function C() {
+function G() {
     const synth = new Tone.Synth().toMaster()
-    synth.triggerAttackRelease('C4', '8n')
+    synth.triggerAttackRelease('G4', '8n')
 }
-function C() {
+function A() {
     const synth = new Tone.Synth().toMaster()
-    synth.triggerAttackRelease('C4', '8n')
+    synth.triggerAttackRelease('A4', '8n')
 }
-function C() {
+function B() {
     const synth = new Tone.Synth().toMaster()
-    synth.triggerAttackRelease('C4', '8n')
+    synth.triggerAttackRelease('B4', '8n')
 }
-function C() {
+function C2() {
     const synth = new Tone.Synth().toMaster()
-    synth.triggerAttackRelease('C4', '8n')
-}
-function C() {
-    const synth = new Tone.Synth().toMaster()
-    synth.triggerAttackRelease('C4', '8n')
-}
-function C() {
-    const synth = new Tone.Synth().toMaster()
-    synth.triggerAttackRelease('C4', '8n')
-}
-function C() {
-    const synth = new Tone.Synth().toMaster()
-    synth.triggerAttackRelease('C4', '8n')
-}
-function C() {
-    const synth = new Tone.Synth().toMaster()
-    synth.triggerAttackRelease('C4', '8n')
-}
-function C() {
-    const synth = new Tone.Synth().toMaster()
-    synth.triggerAttackRelease('C4', '8n')
-}
-function C() {
-    const synth = new Tone.Synth().toMaster()
-    synth.triggerAttackRelease('C4', '8n')
-}
-function C() {
-    const synth = new Tone.Synth().toMaster()
-    synth.triggerAttackRelease('C4', '8n')
-}
-function C() {
-    const synth = new Tone.Synth().toMaster()
-    synth.triggerAttackRelease('C4', '8n')
-}
-function C() {
-    const synth = new Tone.Synth().toMaster()
-    synth.triggerAttackRelease('C4', '8n')
-}
-function C() {
-    const synth = new Tone.Synth().toMaster()
-    synth.triggerAttackRelease('C4', '8n')
-}
-function C() {
-    const synth = new Tone.Synth().toMaster()
-    synth.triggerAttackRelease('C4', '8n')
+    synth.triggerAttackRelease('C5', '8n')
 }
 
+function D2() {
+    const synth = new Tone.Synth().toMaster()
+    synth.triggerAttackRelease('D5', '8n')
+}
+function E2() {
+    const synth = new Tone.Synth().toMaster()
+    synth.triggerAttackRelease('E5', '8n')
+}
+function F2() {
+    const synth = new Tone.Synth().toMaster()
+    synth.triggerAttackRelease('F5', '8n')
+}
+function G2() {
+    const synth = new Tone.Synth().toMaster()
+    synth.triggerAttackRelease('G5', '8n')
+}
+function A2() {
+    const synth = new Tone.Synth().toMaster()
+    synth.triggerAttackRelease('A5', '8n')
+}
+function B2() {
+    const synth = new Tone.Synth().toMaster()
+    synth.triggerAttackRelease('B5', '8n')
+}
